5261bac6b6048d662591af1694c3b3f7a250fd03
Felipefams <augustofams@gmail.com>
2022-03-26 18:29:10 -0300

    02 quase pronto

--- 748473_Felipe_Augusto_Morais_Silva_Guia08/Exemplo_0802.v
+++ 748473_Felipe_Augusto_Morais_Silva_Guia08/Exemplo_0802.v
@@ -1,37 +1,52 @@
 // -------------------------
-// Exemplo_0801 - FULL ADDER
-// Nome: xxx yyy zzz
-// Matricula: 999999
+// Exemplo_0802 - FULL ADDER
+// Nome:Felipe Augusto Morais Silva 
+// Matricula:748473 
 // -------------------------
 // -------------------------
-// half adder
+// half subtractor
 // -------------------------
-module halfAdder (output s1, output s0, input a, input b);
-// descrever por portas
-xor XOR1 ( s0, a, b );
-and AND1 ( s1, a, b );
+module halfSub (output s1, output s0, input a, input b);
+wire a_;
+xor(s0, a,b);
+not(a_,a);
+and(s1,a_,b);
 endmodule // halfAdder
 // -------------------------
 // full adder
-// -------------------------
-module fullAdder ( output s1, output s0, input a, input b, input carryIn );
-// descrever por portas e/ou modulos
-// (valores arbitrarios escolhidos apenas para exemplo)
-not NOT1 ( s1, a ); // valor arbitrario
-not NOT2 ( s0, b ); // valor arbitrario
-endmodule // fullAdder
-module test_fullAdder;
-// ------------------------- definir dados
-reg [3:0] x;
-reg [3:0] y;
-wire [3:0] carry; //vai-um
-wire [4:0] soma;
-// halfAdder HA0 ( carry[0], soma[0], x[0], y[0] );
-fullAdder FA0 ( carry[0], soma[0], x[0], y[0], 1'b0 );
 // ------------------------- parte principal
+module halfSubTeste;
+reg a;
+reg b;
+wire different;
+wire borrow;
+halfSub teste(different, borrow, a, b);
 initial begin
 $display("Exemplo0802 - Felipe Augusto Morais Silva- 748473");
-$display("Test ALU's full adder");
-// projetar testes do somador completo
+$display("Test ALU's full subtractor");
+$display("a   -   b = ans");
+$monitor("%4b + %4b = %4b", a, b, different);
+
 end
 endmodule // test_fullAdder
+// half-adder test_bench by Md. Nahidul Islam
+// module tb_half_adder();
+// reg a, b;
+// wire s, c;
+// half_adder ckt(a,b,s,c);
+//initial 
+//begin
+//	a = 1'b0;
+//	b = 1'b0;
+//	#100
+//	a = 1'b1; 
+//	b = 1'b0;
+//	#100
+//	a = 1'b0; 
+//	b = 1'b1;
+//	#100
+//	a = 1'b1; 
+//	b = 1'b1;
+
+
+//end 
