be5c784ac18ff4ebe1c6840cf029aaa532f6fd29
Felipefams <augustofams@gmail.com>
2022-05-29 18:07:50 -0300

    .

--- teste.v
+++ teste.v
@@ -1,27 +1,24 @@
-module f ( output z , input c, input a, input b);
-wire w1, w2;
-      and AND_1 (z,w1,w2);
-      or  OR__1 (w1,a,b);
-      not NOT_1 (w2,c);
-endmodule
+module f ( output s , input a, input c, input b);
+assign s = !(a&c)||!(b||c);
+endmodule //f
 module test_f;
-reg c;
 reg a;
+reg c;
 reg b;
-wire z;
-f module_test (z, c, a, b);
+wire wire_a;
+f module_test (wire_a, a, c, b);
 initial
 begin : main
 $display("test module");
-$display("    c    a    b =    a ");
-$monitor(" %4b %4b %4b = %4b" , c, a, b, z);
-#1 c=1'b0; a=1'b0; b=1'b0; 
-#1 c=1'b0; a=1'b0; b=1'b1; 
-#1 c=1'b0; a=1'b1; b=1'b1; 
-#1 c=1'b0; a=1'b1; b=1'b0; 
-#1 c=1'b1; a=1'b1; b=1'b0; 
-#1 c=1'b1; a=1'b1; b=1'b1; 
-#1 c=1'b1; a=1'b0; b=1'b1; 
-#1 c=1'b1; a=1'b0; b=1'b0; 
+$display("    a    c    b =    wire_a ");
+$monitor(" %4b %4b %4b = %4b" , a, c, b, wire_a);
+#1 a=1'b0; c=1'b0; b=1'b0; 
+#1 a=1'b0; c=1'b0; b=1'b1; 
+#1 a=1'b0; c=1'b1; b=1'b1; 
+#1 a=1'b0; c=1'b1; b=1'b0; 
+#1 a=1'b1; c=1'b1; b=1'b0; 
+#1 a=1'b1; c=1'b1; b=1'b1; 
+#1 a=1'b1; c=1'b0; b=1'b1; 
+#1 a=1'b1; c=1'b0; b=1'b0; 
 end
 endmodule //test
